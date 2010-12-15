b7c9cafa6ac72e22a5c12959ed2fb6fd517c36db
Lim Chee Kin <limcheekin@vobject.com>
2010-12-15 05:13:29 +0000

    Import images for documentation

--- JqueryValidationUiGrailsPlugin.groovy
+++ JqueryValidationUiGrailsPlugin.groovy
@@ -21,7 +21,7 @@
 */
 class JqueryValidationUiGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "1.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.5 > *"
     // the other plugins this plugin depends on
@@ -33,7 +33,7 @@ class JqueryValidationUiGrailsPlugin {
 
     def author = "Lim Chee Kin"
     def authorEmail = "limcheekin@vobject.com"
-    def title = "JQuery Validation UI Plugin - The Ultimate Grails Client Side Validation"
+    def title = "JQuery Validation UI Plugin - Client Side Validation without writing JavaScript"
     def description = '''\
 Grails Validation mechanism is great, I like it! 
 However, it is just server side validation solution, to build a comprehensive validation solution 
@@ -58,31 +58,4 @@ Javascript Validator + Remote Constraints + jQuery Validation plugin + qTip
 
     // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/jquery-validation-ui"
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before 
-    }
-
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
 }
