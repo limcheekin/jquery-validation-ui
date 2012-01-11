eee218d7590f60bf4d38532635ef8da6e621bba6
bluesliverx <bksaville@gmail.com>
2012-01-11 07:43:54 -0700

    First pages commit

--- grails-app/conf/JqueryValidationUiResources.groovy
+++
@@ -1,9 +0,0 @@
-modules = {
-	'jquery-validation-ui' {
-		dependsOn 'jquery,jquery-validate'
-		resource id:"qtip", url:[plugin:'jqueryValidationUi', dir:'js/qTip', file:'jquery.qtip.pack.js']
-		resource id:"validation-methods", url:[plugin:'jqueryValidationUi', dir:'js/jquery-validation-ui', file:'grails-validation-methods.js']
-		
-		resource id:"qtip-theme", url:[plugin:'jqueryValidationUi', dir:'css/qTip', file:'jquery.qtip.css']
-    }
-}
--- src/groovy/org/grails/jquery/validation/ui/ConstrainedPropertiesEntry.groovy
+++
@@ -1,7 +0,0 @@
-package org.grails.jquery.validation.ui
-
-class ConstrainedPropertiesEntry {
-	String namespace
-	Class validatableClass
-	Map constrainedProperties
-}
