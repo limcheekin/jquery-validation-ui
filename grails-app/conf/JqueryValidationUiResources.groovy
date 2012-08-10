071ec605dae031c06f48f4f98c89b98f1733974a
Lim Chee Kin <limcheekin@vobject.com>
2012-08-10 16:21:05 +0800

    created jquery-validation-ui-qtip module to include qtip resources, the jquery-validation-ui module excluded qtip resources.

--- grails-app/conf/JqueryValidationUiResources.groovy
+++ grails-app/conf/JqueryValidationUiResources.groovy
@@ -1,9 +1,12 @@
 modules = {
 	'jquery-validation-ui' {
 		dependsOn 'jquery, jquery-validate'
-		resource id:"qtip", url:[plugin:'jqueryValidationUi', dir:'js/qTip', file:'jquery.qtip.pack.js']
 		resource id:"validation-methods", url:[plugin:'jqueryValidationUi', dir:'js/jquery-validation-ui', file:'grails-validation-methods.js']
-		
-		resource id:"qtip-theme", url:[plugin:'jqueryValidationUi', dir:'css/qTip', file:'jquery.qtip.css']
     }
+    
+	'jquery-validation-ui-qtip' {
+		dependsOn 'jquery-validation-ui'
+		resource id:"qtip", url:[plugin:'jqueryValidationUi', dir:'js/qTip', file:'jquery.qtip.pack.js']
+		resource id:"qtip-theme", url:[plugin:'jqueryValidationUi', dir:'css/qTip', file:'jquery.qtip.css']
+    }    
 }
