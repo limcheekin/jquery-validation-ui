1122f65393636a9828d9b3a80959e87db735ede1
Lim Chee Kin <limcheekin@vobject.com>
2013-02-22 10:59:32 +0800

    update version no

--- JqueryValidationUiGrailsPlugin.groovy
+++ JqueryValidationUiGrailsPlugin.groovy
@@ -1,4 +1,4 @@
-/* Copyright 2010-2012 the original author or authors.
+/* Copyright 2010-2013 the original author or authors.
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -21,7 +21,7 @@
 */
 class JqueryValidationUiGrailsPlugin {
     // the plugin version
-    def version = "1.4"
+    def version = "1.4.1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2.2 > *"
     // the other plugins this plugin depends on
